4bdc65063bb81552b77ae35a46d25abdf3a82991
penny <liuyang4pub@sina.com>
2020-07-09 06:35:28 +0800

    feat: 增加模拟运动的测试

--- test/test_2body.py
+++ test/test_2body.py
@@ -7,8 +7,6 @@ from simu import Environment, Entity
 from simu import vec
 import numpy as np
 
-K = 9.8  #
-
 
 class Body(Entity):
     """ 运动物体. """
@@ -18,47 +16,52 @@ class Body(Entity):
         self.protect_props.append('pos')
         self.pos = np.zeros(2)
         self.vel = np.zeros(2)
-        self.M = 1.
-        self._F = np.zeros(2)
-        self.access_handlers.append(gravaity_rule)
+        self.m = 1.
+        self._f = np.zeros(2)
 
     def step(self, time_info):
         """ 步进. """
         _, dt = time_info
-        self.vel += dt * self._F
+        self.vel += dt * self._f
         self.pos += self.vel * dt
-        self._F = np.zeros(2)
+        self._f = np.zeros(2)
 
 
 def gravaity_rule(obj: Body, other: Body):
     """ 万有引力定律. """
+    G = 6.67e-11  #
     v = vec.unit(other.pos - obj.pos)
-    f = K * other.M / (vec.dist(obj.pos, other.pos) ** 2)
-    obj._F += f * v
+    f = G * other.m / (vec.dist(obj.pos, other.pos) ** 2)
+    obj._f += f * v
+    pass
+
+
+def chase_rule(obj: Body, other: Body):
+    """ 追逐规律. """
+    d = 1.5  # vec.dist(obj.vel)
+    v = vec.unit(other.pos - obj.pos)
+    obj.vel = d * v
     pass
 
 
 class Recorder:
+    """ 属性记录. """
+
     def __init__(self):
-        self.pos1 = []
-        self.pos2 = []
+        self.records = {}
 
     def __call__(self, env):
-        sat = env.find('sat')
-        if sat:
-            self.pos1.append(sat.pos.copy())
-
-        earth = env.find('earth')
-        if earth:
-            self.pos2.append(earth.pos.copy())
+        for obj in env.entities:
+            if isinstance(obj, Body):
+                if obj.id not in self.records:
+                    self.records[obj.id] = []
+                self.records[obj.id].append(obj.pos.copy())
 
     def plot(self):
         import matplotlib.pyplot as plt
-        xy = np.array(self.pos1).T
-        plt.plot(xy[0,:], xy[1,:], 'g')
-
-        xy = np.array(self.pos2).T
-        plt.plot(xy[0,:], xy[1,:], 'r')
+        for _, pos in self.records.items():
+            xy = np.array(pos).T
+            plt.plot(xy[0, :], xy[1, :])
         plt.show()
 
 
@@ -69,12 +72,31 @@ class BodyTest(unittest.TestCase):
         env.step_events.append(recorder)
 
         sat = env.add(Body('sat'))
-        sat.pos = np.array([0, 1000], dtype=np.float)
-        sat.vel = np.array([20, 0], dtype=np.float)
+        sat.pos = np.array([0, 6000e3], dtype=np.float)
+        sat.vel = np.array([10e3, 0], dtype=np.float)
+        sat.access_handlers.append(gravaity_rule)
 
         earth = env.add(Body('earth'))
-        earth.M = 1e3
+        earth.access_handlers.append(gravaity_rule)
+        earth.m = 5.965e24
 
-        env.run(step=1.0, duration=3000)
-        self.assertTrue(len(recorder.pos1) == len(recorder.pos2) > 0)
+        env.run(step=1.0, duration=30000)
+        self.assertTrue(len(recorder.records) > 0)
+        recorder.plot()
+
+    def test_chase(self):
+        """ 狗追兔子. """
+        env = Environment()
+        recorder = Recorder()
+        env.step_events.append(recorder)
+
+        rabbit = env.add(Body('rabbit'))
+        rabbit.vel = np.array([1, 0], dtype=np.float)
+
+        dog = env.add(Body('dog'))
+        dog.pos = np.array([0, 20], dtype=np.float)
+        dog.access_handlers.append(chase_rule)
+
+        env.run(step=0.1, duration=30)
+        self.assertTrue(len(recorder.records) > 0)
         recorder.plot()
